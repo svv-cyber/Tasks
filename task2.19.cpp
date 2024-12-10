21969ecb223dd9412292ef9f9d0fea7e3af35645
llstepanova <svv0508@gmail.com>
2024-12-10 21:29:06 +0300

    Add files via upload

---
+++ task2.19.cpp
@@ -0,0 +1,30 @@
+﻿/*//Дано четырехзначное число. Найти:
+а)сумму его цифр
+б) произведение его цифр*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	setlocale(0, "rus");
+
+	int number;
+	cout << "Введите четырехзначное число: ";
+	cin >> number;
+	
+	int sum = 0;
+	int multiplication = 1;
+
+	//Вычисление суммы и произведения цифр
+	while (number > 0) {
+		int digit = number % 10;// Извлекаем последнюю цифру
+		sum += digit;   // Добавляем к сумме
+		multiplication *= digit;   // Умножаем на произведение
+		number /= 10;      // Удаляем последнюю цифру
+	}
+	cout << "Сумма цифр: " << sum << endl;
+	cout << "Произведение цифр: " << multiplication << endl;
+
+	return 0;
+}
