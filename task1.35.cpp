98f6cd8ec3320aa4674212353537da82b5ae8814
llstepanova <svv0508@gmail.com>
2024-12-10 21:28:24 +0300

    Add files via upload

---
+++ task1.35.cpp
@@ -0,0 +1,20 @@
+﻿//даны катеты прямоугольного треугольника. Найти его гипотенузу.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+	//Пусть а-перый катет, b-второй катет, с-гипотеуза
+	double a;
+	cout << "Введите значние первого катета: ";
+	cin >> a;
+
+	double b;
+	cout << "Введите значние второго катета: ";
+	cin >> b;
+
+	double hypotenuse = sqrt((a * a) + (b * b)); //Вычисление гипотенузы cout « "Значение гипотенузы: " «< hypotenuse « endl;
+
+	return 0;
+}
