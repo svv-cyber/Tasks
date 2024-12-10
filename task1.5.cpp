263f7e6a9b629b947762761b1c38a192c52a4555
llstepanova <svv0508@gmail.com>
2024-12-10 21:26:49 +0300

    Add files via upload

---
+++ task1.5.cpp
@@ -0,0 +1,15 @@
+﻿//Вывести на одной строке числа 1, 13, 49 с одним пробеломмежду ними
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int firstNumber = 1;
+
+	int secondNumber = 13;
+
+	int thirdNumber = 49;
+
+	cout << firstNumber << " "<< secondNumber << " " << thirdNumber << endl;
+	return 0;
+}
