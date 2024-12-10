7f42bf6de2ffbbe9857c3efb0b02f952e4bdd755
llstepanova <svv0508@gmail.com>
2024-12-10 21:27:53 +0300

    Add files via upload

---
+++ task1.29.cpp
@@ -0,0 +1,21 @@
+﻿// Дан радиус окружности. Найти длину окружности и площадь круга.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	setlocale(0, "rus");
+
+	double Pi = 3.141592653589793;
+	double r;
+
+	cout  << "Введите радиус окружности: ";
+	cin >> r;
+
+	double AreafaCircle = Pi * r * r; //Площадь круга
+	double LenghtOfaCircle = 2 * Pi * r; //Длина окружности
+
+	cout << "Площадь круга: "<< AreafaCircle << endl << "Длина окружности: "<< LenghtOfaCircle << endl;
+	return 0;
+}
