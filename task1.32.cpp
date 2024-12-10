177365a18cea76e515159d0c526120e238263dcb
llstepanova <svv0508@gmail.com>
2024-12-10 21:28:12 +0300

    Add files via upload

---
+++ task1.32.cpp
@@ -0,0 +1,22 @@
+﻿//Известны объем и масса тела. Определить плотность материала этого тела.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+
+	double volume;
+	cout << "Введите значение объёма: ";
+	cin >> volume;
+
+	double weight;
+	cout << "Введите значение массы: ";
+	cin >> weight;
+
+	double materialDensity = volume * weight;
+	cout << "Значение плотности материала: " << materialDensity << endl;
+	return 0;
+}
+
+
