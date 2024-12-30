feae549f4406fc6bfc0a26dee01728ee39e61624
llstepanova <svv0508@gmail.com>
2024-12-30 16:28:50 +0300

    Add files via upload

---
+++ task9.51.cpp
@@ -0,0 +1,33 @@
+﻿//Дано предложение. Напечатать все его буквы и.
+#include <iostream>
+using namespace std;
+#include <string> 
+
+
+int main() 
+{
+    setlocale(0, "rus");
+
+    string sentence;
+    cout << "Введите предложение: ";
+    cin >> sentence;
+
+    int sum = 0;
+    for (int i = 0; i < sentence.length(); i++)
+    {
+        if (sentence[i] == 'и') {
+            sum++;
+        }
+    }
+
+    std::string str_sum = std::to_string(sum);
+
+    cout << "Количество букв (и) в предложении: " + str_sum << std::endl;
+
+    return 0;
+}
+
+
+
+
+
